f7e33cd5a1e16cab5910627d0cc90a331909ca38
Suprim <suprimxpop10@gmail.com>
2026-07-13 19:18:38 +0545

    feat: add ticket repository

---
+++ src/modules/tickets/repository.ts
@@ -0,0 +1,12 @@
+import { Ticket, ITicket } from './model';
+
+export class TicketRepository {
+  async createTicket(data: Partial<ITicket>): Promise<ITicket> {
+    const ticket = new Ticket(data);
+    return await ticket.save();
+  }
+
+  async getMyTickets(userId: string): Promise<ITicket[]> {
+    return await Ticket.find({ user: userId }).populate('tournament', 'title date location');
+  }
+}
