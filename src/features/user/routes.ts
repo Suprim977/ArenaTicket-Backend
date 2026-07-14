88d87aaec4cad78468fac42e46335eb711eff7e7
Suprim <suprimxpop10@gmail.com>
2026-07-14 09:17:14 +0545

    feat: add user routes

---
+++ src/features/user/routes.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import { UserController } from './controller';
+import { authenticate } from '../../middlewares/auth';
+
+const router = Router();
+const userController = new UserController();
+
+router.patch('/profile', authenticate, userController.updateProfile);
+router.patch('/change-password', authenticate, userController.changePassword);
+
+export default router;
