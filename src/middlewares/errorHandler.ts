5908a84484da7c98645695e7e0b9e9251b69be0a
Suprim <suprimxpop10@gmail.com>
2026-07-14 09:12:47 +0545

    feat: add error handler

---
+++ src/middlewares/errorHandler.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from 'express';
+
+export class AppError extends Error {
+  statusCode: number;
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.statusCode = statusCode;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+  let statusCode = 500;
+  let message = 'Internal Server Error';
+
+  if (err instanceof AppError) {
+    statusCode = err.statusCode;
+    message = err.message;
+  }
+
+  res.status(statusCode).json({
+    success: false,
+    message,
+  });
+};
