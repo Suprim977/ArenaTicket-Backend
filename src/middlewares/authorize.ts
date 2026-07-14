abea98d83e94a5c55ea883e5715d4201a2ebf413
Suprim <suprimxpop10@gmail.com>
2026-07-14 09:14:31 +0545

    feat: add authorize middleware

---
+++ src/middlewares/authorize.ts
@@ -0,0 +1,17 @@
+import { Response, NextFunction } from 'express';
+import { AuthRequest } from './auth';
+import { AppError } from './errorHandler';
+
+export const authorize = (...roles: string[]) => {
+  return (req: AuthRequest, res: Response, next: NextFunction): void => {
+    if (!req.user) {
+      return next(new AppError('Authentication required', 401));
+    }
+
+    if (!roles.includes(req.user.role)) {
+      return next(new AppError('You do not have permission to perform this action', 403));
+    }
+
+    next();
+  };
+};
